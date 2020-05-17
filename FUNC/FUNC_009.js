a2fbd2941fbfcdc50e0fa7e01da4ad28bb5ff79c
Nyussay <58169429+Nyussay@users.noreply.github.com>
2020-05-17 17:52:16 +0300

    Update FUNC_009.js

--- FUNC/FUNC_009.js
+++ FUNC/FUNC_009.js
@@ -1,10 +1,8 @@
-{{
-        let y = 0;
+{
         const cc = (x = 0) => () => ++x;
-
-        const bindingCc = cc.call(null, y);
-        Out.log(bindingCc())
-        Out.log(bindingCc())
-        Out.log(bindingCc())
-}
+        
+        const b=cc();
+        Out.log(b());
+        Out.log(b());
+        Out.log(b());
 }
